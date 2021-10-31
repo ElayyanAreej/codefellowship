23d6d195d662a501b00f115842c7e96cbeab00d7
ElayyanAreej <areej.hossein@gmail.com>
2021-11-01 00:26:15 +0200

    l16

---
+++ codefellowship/src/main/java/com/example/codefellowship/ApplicationUserRepository.java
@@ -0,0 +1,6 @@
+package com.example.codefellowship;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicationUserRepository extends CrudRepository<ApplicationUser,Integer> {
+}
